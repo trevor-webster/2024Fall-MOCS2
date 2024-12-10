9fab0340a6498aa6c4666a3ec66946118889f680
trevor <trevor>
2024-12-10 14:26:45 -0500

    proposal created

---
+++ notebooks/# Proposal copy.md
@@ -0,0 +1,11 @@
+# Proposal
+
+to solve the problem of capacity assignment by finding optimal topologies that minimize cost within reliability and delay constraints. 
+The delay is a function of both capacity and flow on a node.
+Finding robustness of topologies will give the reliability constraint.
+
+Cascading failures are to be simu
+compare robustness of network topologies
+Compare a preferential attachment model for varying parameter nu
+Evaluate by average delay per packet
+Reliability/robustness by critical threshold
--- notebooks/net-diffusion-adaptive.py
+++ notebooks/net-diffusion-adaptive.py
@@ -44,3 +44,85 @@ def update():
     g, nextg = nextg, g
 
 pycxsimulator.GUI().start(func=[initialize, observe, update])
+
+
+import pycxsimulator
+from pylab import *
+import networkx as nx
+
+history = []
+hops= {}
+p=0.5
+n = 200
+t=0
+def initialize():
+    global g, nextg
+    g = G_ba.copy()
+
+    for node in g.nodes:
+        g.nodes[node]['packets'] = list()
+        g.nodes[node]['capacity'] = 0
+
+
+    nextg = g.copy()
+    nextg.pos = g.pos
+
+def observe():
+    global g, nextg
+    cla()
+    nx.draw(g, cmap = cm.Spectral, vmin = 0, vmax = 1,
+            node_color = [g.nodes[i]['state'] for i in g.nodes],
+            edge_cmap = cm.binary, edge_vmin = 0, edge_vmax = 1,
+            edge_color = [g.edges[i, j]['weight'] for i, j in g.edges],
+            pos = g.pos)
+    
+
+def update():
+    global g, nextg
+    failed   = []
+    for node in (g.nodes):
+        packets = g.nodes[node]['packets']
+        next_packets=[]
+
+        # remove failed node
+        if len(packets) > g.nodes[node]['capacity']: 
+            nextg.remove_edges_from([(node,neighbor) for neighbor in g.neighbors(node)])            
+            continue
+
+        for packet in packets:
+            o, d, packet_id = packet
+            hops[packet] += 1
+            if d == node:
+                # Packet reaches its destination
+                continue
+            
+            else:
+                next_packets.append(packet)
+            
+        
+        while  len(next_packets) > 0:
+            packet = next_packets.pop()
+            o, d, packet_id = packet
+            path = nx.shortest_path(g, source=node, target=d, weight="weight")      
+            neighbor = path[1]
+            nextg.nodes[neighbor]['packets'].append(packet)
+ 
+
+
+        # generate a packet 
+        die = random.uniform(0, 1)
+        if die<p:
+            dest = np.random.choice(list(g.nodes))
+            packet = (node, dest, step)
+            next_packets.append(packet)
+            hops[packet] = 0
+
+            
+        # Update the next packets for the node
+        nextg.nodes[node]['packets'] = next_packets 
+
+    nextg.pos = nx.spring_layout(nextg, pos = g.pos, iterations = 5)
+    history,append((t,g))
+    g, nextg = nextg, g
+
+pycxsimulator.GUI().start(func=[initialize, observe, update])
